f32a2656f1a0512136ebc53fb46eadfe417aca91
Kevin Ernst <ernstki@mail.uc.edu>
2014-07-15 15:39:26 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+/*
+ * script.js for Prof. Wulf's 14SS_IT3046C
+ * 
+ * Project:  Trash
+ * Author:   Kevin Ernst <ernstki@mail.uc.edu>
+ * Date:     2014-XX-XX
+ * 
+ */
+
+// Typical JavaScript .onload method
+//window.onload = function () {
+  // Your JavaScript code here
+//}; // window.onload
+
+// jQuery method #1
+$(document).ready(function() {
+	
+	
+});
+
+// jQuery shorthand method:
+// $(function() {
+// 	
+// });
