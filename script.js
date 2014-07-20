b8c9be8c4716f96ffaebe86fc29c80954c643621
Kevin Ernst <ernstki@mail.uc.edu>
2014-07-20 19:56:07 -0400

    Checkpoint

--- script.js
+++ script.js
@@ -1,24 +1,43 @@
 /*
  * script.js for Prof. Wulf's 14SS_IT3046C
  * 
- * Project:  Trash
+ * Project:  Lab 07 - HTML5 Canvas
  * Author:   Kevin Ernst <ernstki@mail.uc.edu>
- * Date:     2014-XX-XX
+ * Date:     2014-07-20
  * 
  */
 
-// Typical JavaScript .onload method
-//window.onload = function () {
-  // Your JavaScript code here
-//}; // window.onload
-
-// jQuery method #1
-$(document).ready(function() {
-	
-	
-});
 
 // jQuery shorthand method:
-// $(function() {
-// 	
-// });
+$(function() {
+
+  var g2 = canvas.getContext("2d");
+  var NUM_SIDES = 3;
+  
+
+  function beginDrawing() {
+
+
+  }
+
+  function polygon(x, y, radius, numSides) {  
+    g2.beginPath();
+    var angle = Math.PI * 2 / numSides;
+    g2.translate(x,y);  //make x y the center
+    g2.moveTo(radius,0);
+    
+    for (var i = 1; i < numSides; i++) {
+      g2.lineTo(radius * Math.cos(angle * i),
+                radius * Math.sin(angle * i));
+      }
+      g2.closePath();  // draws the last side
+  } // function polygon(x,y,radius,numSides)
+
+
+
+  $('#canvas').bind('mousedown', function() {
+    window.setTimeout
+  });
+
+
+});
